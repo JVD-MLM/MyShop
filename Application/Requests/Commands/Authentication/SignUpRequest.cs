6e084670fca2f674bc6144369492af899108499a
JM <17jm17@gmail.com>
2025-10-25 09:01:00 +0330

    user entity & signup request

---
+++ Application/Requests/Commands/Authentication/SignUpRequest.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using MyShop.Application.DTOs.Authentication;
+
+namespace MyShop.Application.Requests.Commands.Authentication;
+
+public class SignUpRequest : IRequest<SignUpRequestResponse>
+{
+    public string UserName { get; set; }
+    public string Password { get; set; }
+    public string Email { get; set; }
+}
---
+++ Domain/Entities/Identity/User.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MyShop.Domain.Entities.Identity;
+
+public class User : IdentityUser<Guid>
+{
+    public bool IsAdmin { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public bool IsDeleted { get; set; }
+    public DateTime CreatedAt { get; set; }
+
+    public void SetDelete()
+    {
+        IsDeleted = true;
+    }
+}
